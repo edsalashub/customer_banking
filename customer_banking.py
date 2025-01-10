e80ec57b7a96499c3785c19e5eb8673b8637e5ef
edsalashub <98115285+edsalashub@users.noreply.github.com>
2025-01-10 13:55:17 -0600

    Update customer_banking.py

--- customer_banking.py
+++ customer_banking.py
@@ -21,19 +21,23 @@ def main():
     print(f"Your money in savings for this amount of months: {savings_maturity}")
 
     # Call the create_savings_account function and pass the variables from the user.
-    updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
+    updated_savings_balance, calculated_interest_earned =create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-
+    print(f"The interest earned is {calculated_interest_earned}")
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
-
+    cd_balance=int(input("Set you CD balance"))
+    cd_interest=int(input("Set your interest rate"))
+    cd_maturity=int(input("Set the amount of months"))
     # Call the create_cd_account function and pass the variables from the user.
-    updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
+    create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(f"You earned {calculated_interest_earned} and your updated balance in your cd account is :{updated_savings_balance} for the given {cd_maturity} months")
 
 if __name__ == "__main__":
     # Call the main function.
+    main()
